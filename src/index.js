cd6e7192b1c4407a17de03cac451f9caa9bb2d60
Karel Slajer <karel.slajer@gmail.com>
2022-11-23 19:47:55 +0100

    procisteni testovaci commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import ReactDOM from "react-dom/client"
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import { MDBContainer } from "mdb-react-ui-kit"
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { MDBContainer } from 'mdb-react-ui-kit';
 
-//import "@fortawesome/fontawesome-free/css/all.min.css";
-import "mdb-react-ui-kit/dist/css/mdb.min.css"
-import "./index.css"
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import 'mdb-react-ui-kit/dist/css/mdb.min.css'
+import "./index.css";
 
-import MainMenu, { mainFooter } from "./mainMenu"
-import MainPage from "./mainPage"
-import SolarPage from "./solarPage"
-import SecutityPage from "./securityPage"
-import NetworkPage from "./networkPage"
-import NoPage from "./NoPage"
+import MainMenu, {mainFooter} from "./mainMenu"; 
+import MainPage from "./mainPage";
+import SolarPage from "./solarPage";
+import SecutityPage from "./securityPage";
+import NetworkPage from "./networkPage";
+import NoPage from "./NoPage";
 
 export default function RoutePage() {
   return (
@@ -31,8 +31,8 @@ export default function RoutePage() {
       </BrowserRouter>
       {mainFooter()}
     </MDBContainer>
-  )
+  );
 }
 
-const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(<RoutePage />)
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<RoutePage />);
