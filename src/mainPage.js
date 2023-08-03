4f7d53a2c621d29b1454baa81c6cf1a8bc12dc96
Yanyhoo <76249479+Yanyhoo@users.noreply.github.com>
2023-08-03 08:43:58 +0200

    Úprava textů a grafiky

--- src/mainPage.js
+++ src/mainPage.js
@@ -7,86 +7,104 @@ import {
   MDBRow,
   MDBCol,
 } from "mdb-react-ui-kit"
-import "./mainPage.css" // Importujeme externí CSS soubor pro stylování
+import "./mainPage.css"
 
 function mainPage() {
   return (
     <>
       <MDBRow className="bg-light mb-3">
         <MDBCol>
-          <MDBCard className="h-100 d-flex flex-row align-items-start">
-            <MDBCardImage
-              src="images/officesm.png"
-              position="top"
-              alt="IT řešení"
-              className="w-33" // Zde nastavujeme, aby obrázek zabíral 1/3 šířky
-            />
-            <MDBCardBody>
-              <MDBCardTitle>
-                <a href="./network" className="text-reset">
-                  IT řešení pro společnosti
-                </a>
-              </MDBCardTitle>
-              <MDBCardText>
-                Zajišťujeme kompletní dodávku IT služeb pro společnosti od
-                návrhu, přes vývoj a následnou podporu. Při svých dodávkách se
-                zaměřujeme na zákaznická řešení postavená na komponentách
-                Google, Mikrotik a Cisco.
-              </MDBCardText>
-            </MDBCardBody>
+          <MDBCard className="h-100 card-content">
+            <div className="d-flex flex-row align-items-start">
+              <MDBCardImage
+                src="images/officesm.png"
+                position="top"
+                alt="IT řešení"
+                className="card-image"
+              />
+              <MDBCardBody className="card-text">
+                <MDBCardTitle>
+                  <a href="./network" className="text-reset">
+                    Specializované IT služby od našeho týmu{" "}
+                  </a>
+                </MDBCardTitle>
+                <MDBCardText>
+                  Naše odborná skupina nabízí komplexní IT služby, které
+                  pokrývají spektrum od návrhu přes vývoj až po neustálou
+                  podporu. Naše hluboké znalosti a zkušenosti v oblastech
+                  bankovnictví a telekomunikací nám umožňují přinášet inovativní
+                  přístupy do světa malých a středních podniků. S naším odborným
+                  know-how a zaměřením na zákaznická řešení, jsme připraveni
+                  poskytnout IT služby, které jsou přesně přizpůsobeny potřebám
+                  vaší firmy.
+                </MDBCardText>
+              </MDBCardBody>
+            </div>
           </MDBCard>
         </MDBCol>
       </MDBRow>
       <MDBRow className="bg-light mb-3">
         <MDBCol>
-          <MDBCard className="h-100 d-flex flex-row align-items-start">
-            <MDBCardBody>
-              <MDBCardTitle>
-                <a href="./solar" className="text-reset">
-                  Solární elektárny
-                </a>
-              </MDBCardTitle>
-              <MDBCardText>
-                Pro Váš rodinný dům nabízíme různorodé fotovoltaické střešní
-                systémy jako jsou solární taška, solární panel nebo solární
-                fásáda. K fotovoltaické elektrárně dokážeme nakombinovat další
-                technologie jako je tepelné čerpadlo, bateriové uložiště nebo
-                dobíjecí stanice pro elektromobily.
-                <br />
-                <br />
-                Vybrané technologie navrhujeme vždy na míru na základě prvotní
-                analýzy proveditelnosti daného objektu.
-              </MDBCardText>
-            </MDBCardBody>
-            <MDBCardImage
-              src="images/photoii.png"
-              position="top"
-              alt="Solární elektárny"
-              className="w-33" // Zde nastavujeme, aby obrázek zabíral 1/3 šířky
-            />
+          <MDBCard className="h-100 card-content">
+            <div className="d-flex flex-row-reverse align-items-start">
+              <MDBCardImage
+                src="images/photoii.png"
+                position="top"
+                alt="Solární elektárny"
+                className="card-image"
+              />
+              <MDBCardBody className="card-text">
+                <MDBCardTitle>
+                  <a href="./solar" className="text-reset">
+                    Kvalitní solární systémy na míru{" "}
+                  </a>
+                </MDBCardTitle>
+                <MDBCardText>
+                  Poskytujeme komplexní služby v oblasti solární energie, od
+                  prvotní analýzy až po instalaci. S naším omezeným počtem
+                  projektů ročně zaručujeme nejvyšší úroveň kvality a zákaznické
+                  spokojenosti. Naše nabídka zahrnuje různé typy solárních
+                  systémů, od střešních tašek až po panely a fasády, které lze
+                  integrovat s dalšími technologiemi pro maximalizaci účinnosti.
+                  Náš podrobný návrh je navržen tak, aby splnil všechny
+                  požadavky pro schválení u distribuční sítě a získání dotací.
+                </MDBCardText>
+              </MDBCardBody>
+            </div>
           </MDBCard>
         </MDBCol>
       </MDBRow>
       <MDBRow className="bg-light mb-3">
         <MDBCol>
-          <MDBCard className="h-100 d-flex flex-row align-items-start">
-            <MDBCardImage
-              src="images/homesecurityi.png"
-              position="top"
-              alt="Zabezpečení"
-              className="w-33" // Zde nastavujeme, aby obrázek zabíral 1/3 šířky
-            />
-            <MDBCardBody>
-              <MDBCardTitle>
-                <a href="./security" className="text-reset">
-                  Zabezpečení
-                </a>
-              </MDBCardTitle>
-              <MDBCardText>
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </MDBCardText>
-            </MDBCardBody>
+          <MDBCard className="h-100 card-content">
+            <div className="d-flex flex-row align-items-start">
+              <MDBCardImage
+                src="images/homesecurityi.png"
+                position="top"
+                alt="Zabezpečení"
+                className="card-image"
+              />
+              <MDBCardBody className="card-text">
+                <MDBCardTitle>
+                  <a href="./security" className="text-reset">
+                    Moderní zabezpečovací systémy a automatizace pro domy a
+                    kanceláře
+                  </a>
+                </MDBCardTitle>
+                <MDBCardText>
+                  Náš tým odborníků nabízí pružné, inovativní řešení v oblasti
+                  zabezpečení a automatizace pro domácnosti a menší kancelářské
+                  prostory. Naše rozsáhlé IT znalosti a zkušenosti z
+                  implementace bezpečnostních a evakuačních systémů nám umožňují
+                  vytvářet systémy, které lze v budoucnu jednoduše upravovat a
+                  rozšiřovat. S naším individuálním přístupem se každá zakázka
+                  těší maximální péči, což zajišťuje vysokou kvalitu našich
+                  dodávek a pravidelný servis. Po důkladné analýze vašich potřeb
+                  vytvoříme řešení na míru, které po schválení zrealizujeme pro
+                  vaše maximální pohodlí a efektivitu.
+                </MDBCardText>
+              </MDBCardBody>
+            </div>
           </MDBCard>
         </MDBCol>
       </MDBRow>
